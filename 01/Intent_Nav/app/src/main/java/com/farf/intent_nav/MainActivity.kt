97604e6eeae17cfa9516d1d9f8eff4e5856539c4
okwhy <59335640+okwhy@users.noreply.github.com>
2024-07-08 10:26:57 +0400

    Initial commit

---
+++ 01/Intent_Nav/app/src/main/java/com/farf/intent_nav/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.farf.intent_nav
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.farf.intent_nav.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.buttonNav.setOnClickListener{
+            val intent = SecondActivity.newIntent(this,"Some TEXTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
+            startActivity(intent)
+        }
+    }
+}
---
+++ 01/Intent_Nav/app/src/main/java/com/farf/intent_nav/SecondActivity.kt
@@ -0,0 +1,45 @@
+package com.farf.intent_nav
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.farf.intent_nav.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    companion object {
+        private const val EXTRA_MESSAGE = "MESSAGE"
+        fun newIntent(context: Context, message: String): Intent {
+            return Intent(context, SecondActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, message)
+            }
+        }
+    }
+
+    private var toast: Toast? = null
+    private val binding by lazy { ActivitySecondBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        binding.buttonNotify.setOnClickListener {
+            toast?.cancel()
+            toast = Toast.makeText(this, message, Toast.LENGTH_SHORT).apply {
+                show()
+            }
+        }
+        val button=binding.buttonNotify
+        val outValue = TypedValue()
+        button.context.theme.resolveAttribute(android.R.attr.buttonStyle, outValue, true)
+        Log.d("ButtonStyleCheck", button.toString())
+        if (outValue.resourceId != 0) {
+            val buttonStyleName = resources.getResourceEntryName(outValue.resourceId)
+            Log.d("ButtonStyleCheck", "Button style is: $buttonStyleName")
+        } else {
+            Log.d("ButtonStyleCheck", "Button style not found")
+        }
+    }
+}
