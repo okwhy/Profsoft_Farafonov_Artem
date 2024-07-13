bd841a201e59b70077e9f8795955ed8098dd05f9
okwhy <59335640+okwhy@users.noreply.github.com>
2024-07-13 20:11:51 +0400

    03 figma layout

---
+++ 03/Layout_Drawing/app/src/main/java/com/farf/layoutdrawing/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.farf.layoutdrawing
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.farf.layoutdrawing.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+    }
+}
